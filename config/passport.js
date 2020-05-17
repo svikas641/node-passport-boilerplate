1aaa457f3021dd26a80c078464d26a9365fed63e
Vikas_Kumar <svikas641@gmail.com>
2020-05-17 20:11:20 +0530

    first commit

---
+++ config/passport.js
@@ -0,0 +1,37 @@
+const LocalStrategy = require('passport-local').Strategy;
+
+// Load User model
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      // Match user
+      User.findOne({
+        email: email
+      }).then(user => {
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered' });
+        }
+
+        // Match password
+        if(password === user.password){
+            return done(null, user);
+          }
+           else {
+            return done(null, false, { message: 'Password incorrect' });
+          }        
+      });
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
